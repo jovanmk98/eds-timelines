f50e237765405f9d42d47fac423ff5a636db49cf
Jovan <jovan.dimovski@codeland.it>
2025-06-13 00:54:52 +0200

    testttt

--- blocks/timeline/timeline.js
+++ blocks/timeline/timeline.js
@@ -1,6 +1,6 @@
 
 export default async function decorate(block) {
-    console.log("Timeline ", block);
+    console.log("Timeline 123 ", block);
   // 1) mark it as "timeline block" so EDS knows about it (if not already)
   block.classList.add('timeline', 'block');
 }
