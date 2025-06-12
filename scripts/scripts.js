ed2008dee6f3c08e76f4a91ffffba0191254de0b
Jovan <jovan.dimovski@codeland.it>
2025-06-13 00:33:47 +0200

    console-addeD

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -67,7 +67,8 @@ export function decorateMain(main) {
   decorateBlocks(main);
   console.log('Decorating main...', main);
     main.querySelectorAll('div.default-content-wrapper').forEach(wrapper => {
- console.log('Decorating wrapper.123..', wrapper);
+ console.log('Decorating wrapper.12333..', wrapper);
+ wrapper.classList.add('timeline', 'block');
     if (wrapper.querySelector('h1#timeline-of-html')) {
       console.log('Found timeline wrapper');
       // wrapper.parentElement is the <div> inside the section
